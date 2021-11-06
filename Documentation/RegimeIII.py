8c34b9209e43227318cb7a3c81abc780c8edb0fd
Richard Fitzpatrick <rfitzp@farside.ph.utexas.edu>
2021-11-06 11:15:07 -0500

    Added more to tearing mode writeup

---
+++ Documentation/RegimeIII.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+D = 1.
+
+D2 = D*D
+D4 = D2*D2
+D6 = D4*D2
+
+def f1(q):
+    return q*q/D2
+
+def f2(q):
+    return q**1.5
+
+def f3(q):
+    return q*D2
+
+t1 = np.arange(0., D4, 1.e-3)
+t2 = np.arange(D4, 2., 1.e-3)
+t3 = np.arange(D4, 2., 1.e-3)
+
+plt.hlines(y=D6, xmin=0, xmax=D4, linewidth=2, color='blue')
+plt.plot(t1, f1(t1), linewidth=2, color='blue')
+plt.plot(t2, f2(t2), linewidth=2, color='blue')
+plt.plot(t3, f3(t3), linewidth=2, color='blue')
+
+plt.text(0.4, 2.0, 'VR',  fontsize=20)
+plt.text(1.5, 0.5, 'SC',  fontsize=20)
+plt.text(0.2, 0.5, 'HR',  fontsize=20)
+plt.text(1.75, 2.0, 'RI',  fontsize=20)
+
+plt.text(1.29, 2.0, r'$P=Q_\ast^{3/2}$', fontsize=20, color='red')
+plt.text(0.35, 1.03, '$P=D^6$', fontsize=20, color='red')
+plt.text(0.741, 0.4, r'$P=Q_\ast^2 D^{-2}$', fontsize=20, color='red')
+plt.text(1.525, 1.4, r'$P=Q_\ast D^2$', fontsize=20, color='red')
+
+plt.xlabel(r'$Q_\ast$', fontsize=16)
+plt.ylabel('$P$', fontsize=16)
+
+plt.savefig("RegimeIII.pdf")
+
+#plt.show()
